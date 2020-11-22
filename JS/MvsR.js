21ab1eba462ee118ca7b0bba1b63e3119dafcb36
landaverdes <slandaverde307@gmail.com>
2020-11-22 02:37:39 -0500

    added code

--- JS/MvsR.js
+++ JS/MvsR.js
@@ -1,73 +1,114 @@
-var drawBar = function(data,target, graphDim, xScale,yScale){
+var initgraph = function(data) {
     
-    target.selectAll("rect")
+var margin = {top: 30, right: 40, bottom: 50, left: 60},
+    width = 800 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
+
+
+var svg = d3.select("#graph1")
+  .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
+
+    var yScale = d3.scaleLinear()
+    .domain([0, 50])
+    .range([0, height])
+    
+    var subgroups = data.columns.slice(1)
+
+var names = [];
+    data.forEach(season);
+    
+    function season(item, index) {
+        names.push(item.Season)
+    }
+
+
+ var x = d3.scaleBand()
+      .domain(names)
+      .range([0, width])
+      .padding([0.2])
+
+    
+   var groups = d3.map(data, function(d){return(d.CL_Goals)}).keys()
+    
+ 
+  svg.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x).tickSize(0));
+    
+var y = d3.scaleLinear()
+    .domain([0, 50])
+    .range([ height, 0 ]);
+  svg.append("g")
+    .call(d3.axisLeft(y));
+
+     var xSubgroup = d3.scaleBand()
+    .domain(subgroups)
+    .range([0, x.bandwidth()])
+    .padding([0.05])
+     
+       var color = d3.scaleOrdinal()
+    .domain(subgroups)
+    .range(['green','blue',])
+       
+      svg.append("g")
+    .selectAll("g")
+    
+    // Enter in data = loop group per group
     .data(data)
     .enter()
-    .append("rect")
-    .attr("x", function(season) {
-        return xScale(season.Season);
-    })
-    .attr("y", function(season) {
-        return yScale(season.CL_Goals);
-    })
-    .attr("width", 100)
-    .attr("height", function(season) {
-        return graphDim.height - yScale(season.CL_Goals);
-    })
-    
-    
-    
-}
-
-
-
-
-var initGraph = function(data) {
-    
-    var screen = { width: 400, height: 400}
-    var margins = {left:80,right:80,top:30,bottom:30}
-    
-    d3.select("svg")
-    .attr("width", screen.width)
-    .attr("height", screen.height)
-    
-     var target = d3.select("svg")
     .append("g")
-    .attr("id","#graph1")
-    .attr("transform",
-          "translate("+margins.left+","+
-                        margins.top+")");
+      .attr("transform", function(d) { return "translate(" + x(d.Season) + ",0)"; })
     
     
-var xScale = d3.scaleBand()
-.domain([0,18])
-.range([0,screen.width])
+    
+    //bars
+    .selectAll("rect")
+    .data(function(d) { return subgroups.map(function(key) { return {key: key, CL_Goals: d[key]}; }); })
+    .enter().append("rect")
+      .attr("x", function(d) { return xSubgroup(d.key); })
+      .attr("y", function(d) { return y(d.CL_Goals); })
+      .attr("width", xSubgroup.bandwidth())
+      .attr("height", function(d) {return yScale(parseInt(d.CL_Goals));})
+      .attr("fill", function(d) { return color(d.key); });
 
-var yScale = d3.scaleLinear()
-.domain([0,50])
-.range([screen.width, 0])
+
+var labels = d3.select("svg")
+.append("g")
+.classed("labels", "true");
     
-drawBar(data,target, screen, xScale,yScale);
+    labels.append("text")
+    .text("Champions league goals")
+    .classed("title", "true")
+    .attr("text-anchor", "middle")
+    .attr("x", margin.left+(width/2))
+    .attr("y", margin.top-5)
     
+    labels.append("text")
+    .text("Season year")
+    .classed("label", "true")
+    .attr("text-anchor", "middle")
+    .attr("x", margin.left+width/2)
+    .attr("y", height+(margin.bottom+margin.top-5))
     
-    
-    
+    labels.append("g")
+    .attr("transform", "translate(20,"+(margin.top+(height/2))+")")
+    .append("text")
+    .text("Goals")
+    .classed("label", true)
+    .attr("text-anchor", "middle")
+    .attr("transform", "rotate(90)")
+
 }
-
-
-
-
-
-
-
-
-
-
 
 
 var successFNC = function(data) {
     console.log("Data", data);
-    initGraph(data);
+    initgraph(data); 
 }
 var failFNC = function(error) {
     console.log("Error finding data", error);
